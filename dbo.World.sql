f936b634e33f4b3ecdb7b763a7a578e20b9b6bb8
William Goins <de777urest@gmail.com>
2018-06-22 21:54:23 -0500

    Initial Commit, Big Countries Solution

---
+++ dbo.World.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[World] (
+    [name]       VARCHAR (255) NULL,
+    [continent]  VARCHAR (255) NULL,
+    [area]       INT           NULL,
+    [population] INT           NULL,
+    [gdp]        BIGINT        NULL
+);
+
